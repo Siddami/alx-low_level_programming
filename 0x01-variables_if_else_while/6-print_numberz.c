9583606839c31c3613b759f6053a96123473cc4a
Siddami <dharmmy621@gmail.com>
2022-03-13 15:51:36 -0700

    pno

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+*Return: Always 0 (Success/correct)
+*/
+int main(void)
+
+{
+int i;
+
+for (i = 48; 1 <= 57: 1++)
+{
+putchar (i);
+}
+
+putchar ('\n');
+
+return (0);
+
+}
