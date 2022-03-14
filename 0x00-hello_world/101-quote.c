25c6793ff71b60efe1e2ed584354b9ca1eb13ff5
Siddami <dharmmy621@gmail.com>
2022-03-13 17:34:36 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*
+*main - Entry point
+*
+*Return: return 1 after the function
+*/
+int main(void)
+
+{
+int Dora_Korpar;
+
+putchar("and that piece of art is useful\n",n)
+
+return (1);
+
+}
+
