d5d825bbc7ec11773d55920e30d4331907336d86
Siddami <dharmmy621@gmail.com>
2022-03-15 17:33:23 -0700

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,9 +27,9 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(k / 10 + '0')
+				_putchar(k / 10 + '0');
 
-				_putchar(k % 10 + '0')
+				_putchar(k % 10 + '0');
 			}
 		
 		}
