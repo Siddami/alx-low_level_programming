6e2e65adb9b7ab72e4aeaa11fa966b289481eb0c
Siddami <dharmmy621@gmail.com>
2022-03-16 17:10:43 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,7 @@ void print_times_table(int n)
 	{
 		for (row = 0; row <= n; row++)
 		{
-			for (col = 0; col <=n; col++)
+			for (col = 0; col <=n ; col++)
 			{
 				mult = (row * col);
 				if (mult < 10)
