a310e123987c7740170d30caa533cf225613a0e4
Siddami <dharmmy621@gmail.com>
2023-10-08 12:29:43 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,9 @@ int main(void)
 	printf("Size of float: %zu byte(s)\n", sizeof(float));
 	printf("Size of double: %zu byte(s)\n", sizeof(double));
 	printf("Size of long: %zu byte(s)\n", sizeof(long));
+	printf("Size of short: %zu byte(s)\n", sizeof(short));
+	printf("Size of void: %zu byte(s0\n", sizeof(void));
 	printf("Size of long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
 	return (0);
 }
