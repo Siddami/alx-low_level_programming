89563f124c82052b5c7995907155b21c22324444
Siddami <dharmmy621@gmail.com>
2023-10-09 11:55:00 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,26 +7,15 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('\n');
-	putchar('1');
-	putchar('\n');
-	putchar('2');
-	putchar('\n');
-	putchar('3');
-	putchar('\n');
-	putchar('4');
-	putchar('\n');
-	putchar('5');
-	putchar('\n');
-	putchar('6');
-	putchar('\n');
-	putchar('7');
-	putchar('\n');
-	putchar('8');
-	putchar('\n');
-	putchar('9');
-	putchar('\n');
+	char digit;
 
+	digit = '0';
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n');
 	return (0);
 }
