f33e3fcb01f0e6d0fa2763804fbb25cb519f0e06
Siddami <dharmmy621@gmail.com>
2024-01-31 17:39:27 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: number
+ *
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
