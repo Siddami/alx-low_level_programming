16690794e4ad1b0a6ea7b08463cb57bad5ae1341
Siddami <dharmmy621@gmail.com>
2024-01-31 18:00:23 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - prints a square of hastag depending of the num of param
+ *@size: int number
+ *
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size ; b++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
