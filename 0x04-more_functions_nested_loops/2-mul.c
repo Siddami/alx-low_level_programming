a55a72b33fdf1cdab099bcdbda4676515ab7dd44
Siddami <dharmmy621@gmail.com>
2024-01-31 17:21:19 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul- do a multilicatin between two digits
+ *@a: int with a number
+ *@b: int with a number
+ *
+ * Return: 1 on success
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
